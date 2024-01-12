2e592da9f717cb0722880bf9f3db7eaffe358e7f
Prerna Sehgal <prerna@cloudeq.com>
2024-01-12 14:06:26 +0530

    updated

---
+++ src/Pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h2>about page</h2>
+        <p>welcome to about page</p>
+
+    </div>
+  )
+}
---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h2>home page</h2>
+        <p>welcome to home page</p>
+    </div>
+  )
+}
